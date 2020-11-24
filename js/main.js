f855fc8bccdca1cc0282b279c1fb1e1f5d73fd18
Olga Barishevskaya <bariolga@yandex.ru>
2020-11-24 16:39:07 +0300

    Сделала задание 4 доп

--- js/main.js
+++ js/main.js
@@ -1,68 +1,15 @@
 "use strict";
 
-//----------------------Задание 1----------------------//
+function cutString(string, number) {
+    if (typeof string !== "string") {
+        console.log("Это не строка!!")
+    }
+    let newString = string.trim();
 
-let lang = "ru";
-
-let arrRus = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-let arrEng = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-
-//через if
-if (lang === "ru") {
-    console.log(String(arrRus))
-} else if (lang === "en") {
-    console.log(String(arrEng))
-} else {
-    console.log("Произошла ошибка")
-};
-
-
-//через if второй вариант
-
-function getRandomDay(arr) {
-    let num = Math.floor(Math.random() * arr.length);
-    let randDay = arr[num];
-    return randDay
-};
-
-
-if (lang === "ru") {
-    console.log(`Ура, сегодня ${getRandomDay(arrRus)}`)
-} else if (lang === "en") {
-    console.log(`Hurray, today is ${getRandomDay(arrEng)}`)
-} else {
-    console.log("Произошла ошибка")
-};
-
-
-//через switch-case  
-
-switch (lang) {
-    case "ru":
-        console.log(String(arrRus));
-        break;
-    case "en":
-        console.log(String(arrEng));
-        break;
-    default:
-        console.log("Произошла ошибка");
+    if (newString.length > number) {
+        newString = newString.slice(0, number) + '...'
+    }
+    return newString
 }
 
-
-//через многомерный массив без ифов и switch
-
-let arrLang = [
-    ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
-    ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-];
-
-lang === "ru" ? console.log(arrLang[0]) : lang === "en" ? console.log(arrLang[1]) : console.log("Произошла ошибка");
-
-
-
-//----------------------Задание 2----------------------//
-
-let namePerson = "Артем";
-
-namePerson === "Артем" ? console.log("директор") : namePerson === "Максим" ? console.log("преподаватель") : console.log("студент");
-
+console.log(cutString('  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo ', 30));
