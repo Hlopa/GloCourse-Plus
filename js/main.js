531d87cee72961c4e842541dee7233b3b4b4d36f
Olga Barishevskaya <bariolga@yandex.ru>
2020-11-25 16:32:03 +0300

    Сделала задание 5

--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
 "use strict";
 
-/*Я решила сделать функкцию. которая будет обрезать строку на заданное пользователем кол-во соиволов, 
- а не только на 30ть */
 
-function cutString(string, number) {
-    if (typeof string !== "string") {
-        console.log("Это не строка!!")
-    }
-    let newString = string.trim();
+//задание 1
 
-    if (newString.length > number) {
-        newString = newString.slice(0, number) + '...'
+let arr = ['456554', '-407', '10784', '55', '95456558', '652', '2465'];
+
+for (let num of arr) {
+    if (num[0] === '4' || num[0] === '2') {
+        console.log(num)
+    } else if ((num[0] === '-' || num[1] === '4') && (num[0] === '-' || num[1] === '2')) {
+        console.log(num)
     }
-    return newString
+};
+
+
+
+//задание 2
+
+
+foundPrime:
+for (let i = 2; i <= 100; i++) {
+
+    for (let j = 2; j < i; j++) {
+        if (i % j == 0) continue foundPrime;
+    }
+    console.log(`${i} Делители числа: 1 и ${i}`);
 }
-
-console.log(cutString('  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo ', 30));
-
-
-//А вот если соблюдать задание:
-
-function cutString2 (string) {
-    if (typeof string !== "string") {
-        console.log("Это не строка!!")
-    }
-    let newString = string.trim();
-
-    if (newString.length > 30) {
-        newString = newString.slice(0, 30) + '...'
-    }
-    return newString
-}
-
-console.log(cutString2('  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo '));
