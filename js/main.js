87984e324233808f37d340059b7e2d0f0c71e2e6
Olga Barishevskaya <bariolga@yandex.ru>
2020-12-09 19:05:08 +0300

    Сделала задание 14 доп

--- js/main.js
+++ js/main.js
@@ -25,11 +25,39 @@ DomElement.prototype.createElem = function () {
     width: ${this.width}px;
     background-color: ${this.bg};
     font-size: ${this.fontSize}px;
-    `
+    `;
     elem.textContent = this.text;
     document.body.append(elem);
     return elem
 };
 
+
 const newElem = new DomElement('.block', 50, 150, 'green', 20, 'hello');
 newElem.createElem();
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    const square = new DomElement('.square', 100, 100, 'red').createElem();
+    square.style.position = 'absolute';
+
+    document.addEventListener('keydown', function (e) {
+        let styleSquare = getComputedStyle(square);
+        let topSquare = parseFloat(styleSquare.top);
+        let rightSquare = parseFloat(styleSquare.right);
+
+        const keyName = e.code;
+
+        if (keyName === 'ArrowUp') {
+            topSquare -= 10;
+            square.style.top = topSquare + 'px';
+        } else if (keyName === 'ArrowDown') {
+            topSquare += 10;
+            square.style.top = topSquare + 'px';
+        } else if (keyName === 'ArrowLeft') {
+            rightSquare += 10;
+            square.style.right = rightSquare + 'px';
+        } else if (keyName === 'ArrowRight') {
+            rightSquare -= 10;
+            square.style.right = rightSquare + 'px';
+        }
+    });
+});
